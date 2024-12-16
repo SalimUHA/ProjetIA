40026cc7db84d0c61cbad9db798a55f6291eedb9
SalimUHA <salimahmida68@gmail.com>
2024-12-16 11:03:54 +0100

    Agent

---
+++ ProjetACOR/include/Agent.h
@@ -0,0 +1,22 @@
+#ifndef AGENT_H
+#define AGENT_H
+
+
+#include <vector>
+#include <iostream>
+
+class Agent {
+public:
+	Agent();
+	Agent(std::vector<double>& solution);
+	Agent& operator=(const Agent& other);
+	void set_solution(std::vector<double>& solution);
+	void set_fitness(double fitness);
+	double getfitness() const;
+	std::vector<double> getsolution() const;
+private:
+	std::vector<double> d_solution;
+	double d_fitness;
+};
+
+#endif // AGENT_H
